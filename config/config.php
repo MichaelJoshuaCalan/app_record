3cb1bfa83583a8a4433f22359ac150100c4e08bc
Clyduuu <202080146@psu.palawan.edu.ph>
2023-11-12 00:44:54 +0800

    Done Changes

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+define('ROOT_URL', 'localhost/app_record/');
+define('DB_HOST', 'localhost:3306');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'records_app');
+
---
+++ config/db.php
@@ -0,0 +1,23 @@
+<?php
+// Database configuration
+$host = 'localhost:3306';  // Replace with your actual database host
+$user = 'root';  // Replace with your actual database username
+$pass = '';  // Replace with your actual database password
+$db = 'records_app';  // Replace with your actual database name
+
+// Create Connection
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Set charset to utf8mb4 (if needed)
+$conn->set_charset("utf8mb4");
+
+// You can also enable error reporting for debugging purposes
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+// Now you can use the $conn variable for database operations
+?>
