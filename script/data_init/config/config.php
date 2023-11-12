ae7149ad46e15e08f446435e85b0dca726aef55e
MichaelJoshuaCalan <mr.swabe.xx12345@gmail.com>
2023-11-12 17:09:40 +0800

    crud

---
+++ script/data_init/config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', 'http://localhost/script/data_init/');
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', 'feb62002');
+	define('DB_NAME', 'record_app');
---
+++ script/data_init/includes/sidebar.php
@@ -0,0 +1,35 @@
+<div class="logo">
+                    <a href="javascript:;" class="simple-text">
+                        Your logo
+                    </a>
+                </div>
+                <ul class="nav">
+                    <li>
+                        <a class="nav-link" href="transactions.php">
+                            <i class="nc-icon nc-chart-pie-35"></i>
+                            <p>Transactions</p>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a class="nav-link" href="office.php">
+                            <i class="nc-icon nc-bank"></i>
+                            <p>Office</p>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a class="nav-link" href="employee.php">
+                            <i class="nc-icon nc-single-02"></i>
+                            <p>Employee</p>
+                        </a>
+                    </li>
+                    
+                    
+                    <li class="nav-item active active-pro">
+                        <a class="nav-link active" href="javascript:;">
+                            <i class="nc-icon nc-single-02"></i>
+                            <p>Upgrade Plan</p>
+                        </a>
+                    </li>
+                </ul>
